040aaf4fcf927743ec61534555e2f5f440ad80c3
cheifaaaaaa <cheifaikram@gmail.com>
2023-06-21 20:40:42 +0100

    commit

--- admin/admin_header.php
+++ admin/admin_header.php
@@ -1,3 +1,6 @@
+
+<!--=============== BOXICONS ===============-->
+<link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
 <header class="header">
 
    <div class="flex">
@@ -5,16 +8,16 @@
       <a href="admin_page.php" class="logo">Admin<span>Panel</span></a>
 
       <nav class="navbar">
-         <a href="admin_page.php">home</a>
-         <a href="admin_products.php">products</a>
-         <a href="admin_orders.php">orders</a>
-         <a href="admin_users.php">users</a>
-         <a href="admin_contacts.php">messages</a>
+         <a href="admin_page.php">Home</a>
+         <a href="admin_products.php">Products</a>
+         <a href="admin_orders.php">Orders</a>
+         <a href="admin_users.php">Users</a>
+         <a href="admin_contacts.php">Messages</a>
       </nav>
 
       <div class="icons">
-         <div id="menu-btn" class="fas fa-bars"></div>
-         <div id="user-btn" class="fas fa-user"></div>
+         <i class='bx bxs-user'></i>
+         <i class='bx bx-menu'></i>
       </div>
 
       <!-- <div class="account-box">
