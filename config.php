b3d967db15db84ce857d46b4586d8e89fac049c8
cheifaikram <cheifaikram@gmail.com>
2023-06-26 00:43:31 +0100

    commit

--- config.php
+++ config.php
@@ -1,5 +1,9 @@
 <?php
 
 $conn = mysqli_connect('localhost:3307','root','','shopping_db') or die('connection failed');
-
+if (mysqli_connect_error()) {
+    die('Database connection error: ' . mysqli_connect_error());
+}
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
 ?>
