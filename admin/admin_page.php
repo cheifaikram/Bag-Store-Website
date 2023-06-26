c6ab5f5aaecbc674db200f4830483ff76668de95
cheifaikram <cheifaikram@gmail.com>
2023-06-26 05:12:06 +0100

    commit

--- admin/admin_page.php
+++ admin/admin_page.php
@@ -17,63 +17,7 @@ session_start();
 <!-- admin dashboard section starts  -->
 
 <section class="main">
-        <!-- <div class="sidebar">
-            <ul class="sidebar--items">
-                <li>
-                    <a href="#" class="active">
-                        <span class="icon"><i class="ri-bar-chart-line"></i></span>
-                        <div class="sidebar--item">Overview</div>
-                    </a>
-                </li>
-                <li>
-                    <a href="#">
-                        <span class="icon"><i class="ri-handbag-line"></i></span>
-                        <div class="sidebar--item">Product</div>
-                    </a>
-                </li>
-                <li>
-                    <a href="#">
-                        <span class="icon"><i class="ri-user-line"></i></span>
-                        <div class="sidebar--item">Customers</div>
-                    </a>
-                </li>
-                <li>
-                    <a href="#">
-                        <span class="icon"><i class="ri-booklet-line"></i></span>
-                        <div class="sidebar--item">Orders</div>
-                    </a>
-                </li>
-                <li>
-                    <a href="#">
-                        <span class="icon"><i class="ri-shopping-cart-2-line"></i></span>
-                        <div class="sidebar--item">Checkout</div>
-                    </a>
-                </li>
-                <li>
-                    <a href="#">
-                        <span class="icon"><i class="ri-settings-3-line"></i></span>
-                        <div class="sidebar--item">Settings</div>
-                    </a>
-                </li>
-            </ul>
-            <ul class="sidebar--bottom--items">
-                <li>
-                    <a href="#">
-                        <span class="icon"><i class="ri-question-line"></i></span>
-                        <div class="sidebar--item">Help</div>
-                    </a>
-                </li>
-                <li>
-                    <a href="#">
-                        <span class="icon"><i class="ri-logout-box-r-line"></i></span>
-                        <div class="sidebar--item">Logout</div>
-                    </a>
-                </li>
-            </ul>
-        </div> -->
-
-
-        <div class="main--container">
+    <div class="main--container">
             <div class="section--title">
                 <h3 class="title">Welcome back, kim</h3>
             </div>
@@ -81,59 +25,124 @@ session_start();
                 <div class="card card-1">
                     <div class="card--title">
                         <span class="card--icon icon"><i class='bx bxs-hourglass-bottom'></i></span>
-                        <span>Total Pendings</span>
-                        <?php 
-                        $total_pendings = 0;
-                        $select_pending = mysqli_query($conn, "SELECT total_price FROM `orders` WHERE payement_status = 'pending'") or die('query failed');
-                        if(mysqli_num_rows($select_pending) > 0){
-                            while($fetch_pendings = mysqli_fetch_assoc($select_pending)){
-                               $total_price = $fetch_pendings['total_price'];
-                               $total_pendings += $total_price;
-                            };
-                        };?>
-                        <h3>$<?php echo $total_pendings; ?>/-</h3>
+                        <div>Total Pendings</div>
+                    </div>
+                    <?php 
+                            $total_pendings = 0;
+                            $select_pending = mysqli_query($conn, "SELECT total_price FROM `orders` WHERE payement_status = 'pending'") or die('query failed');
+                            if(mysqli_num_rows($select_pending) > 0){
+                                while($fetch_pendings = mysqli_fetch_assoc($select_pending)){
+                                        $total_price = $fetch_pendings['total_price'];
+                                        $total_pendings += $total_price;
+                                    };
+                            };                 
+                    ?>
+                    <div class="overall">
+                        <h1 class="card--value">$<?php echo $total_pendings; ?>/-</h1>
                     </div>
                 </div>
+
                 <div class="card card-2">
                     <div class="card--title">
                         <span class="card--icon icon"><i class='bx bx-check-square'></i></span>
                         <span>Completed Payments</span>
                     </div>
+                    <?php
+                    $total_completed = 0;
+                    $select_completed = mysqli_query($conn, "SELECT total_price FROM `orders` WHERE payement_status = 'completed'") or die('query failed');
+                    if(mysqli_num_rows($select_completed) > 0){
+                       while($fetch_completed = mysqli_fetch_assoc($select_completed)){
+                          $total_price = $fetch_completed['total_price'];
+                          $total_completed += $total_price;
+                       };
+                    };
+                 ?>
+                 <div class="overall">
+                    <h1 class="card--value">$<?php echo $total_completed; ?>/-</h1>
+                 </div>
                 </div>
+
                 <div class="card card-3">
                     <div class="card--title">
                         <span class="card--icon icon"><i class='bx bx-cart'></i></span>
                         <span>Orders Placed</span>
                     </div>
+                    <?php 
+                    $select_orders = mysqli_query($conn, "SELECT * FROM `orders`") or die('query failed');
+                    $number_of_orders = mysqli_num_rows($select_orders);
+                    ?>
+                    <div class="overall">
+                    <h1 class="card--value"><?php echo $number_of_orders; ?></h1>
+                    </div>   
                 </div>
+
                 <div class="card card-4">
                     <div class="card--title">
                         <span class="card--icon icon"><i class='bx bx-cart-add'></i></span>
                         <span>Products Added</span>
                     </div>
+                    <?php 
+                      $select_products = mysqli_query($conn, "SELECT * FROM `products`") or die('query failed');
+                      $number_of_products = mysqli_num_rows($select_products);
+                    ?>
+                    <div class="overall">
+                       <h1 class="card--value"><?php echo $number_of_products; ?></h>
+                    </div>     
                 </div>
+
                 <div class="card card-4">
                     <div class="card--title">
                         <span class="card--icon icon"><i class='bx bx-user'></i></span>
                         <span>Normal Users</span>
                     </div>
+
+                    <?php 
+                      $select_users = mysqli_query($conn, "SELECT * FROM `users` WHERE user_type = 'user'") or die('query failed');
+                      $number_of_users = mysqli_num_rows($select_users);
+                    ?>
+                    <div class="overall">
+                       <h1 class="card--value"><?php echo $number_of_users; ?></h1>
+                    </div>    
                 </div>
+
                 <div class="card card-3">
                     <div class="card--title">
                         <span class="card--icon icon"><i class='bx bxs-user-check'></i></span>
                         <span>Admins</span>
                     </div>
+                    <?php 
+                      $select_admins = mysqli_query($conn, "SELECT * FROM `users` WHERE user_type = 'admin'") or die('query failed');
+                      $number_of_admins = mysqli_num_rows($select_admins);
+                    ?>
+                    <div class="overall">
+                    <h1 class="card--value"><?php echo $number_of_admins; ?></h1>
+                    </div>
                 </div>
+
                 <div class="card card-2">
                     <div class="card--title">
                         <span class="card--icon icon"><i class='bx bxs-user-account'></i></span>
                         <span>Total Accounts</span>
+                    </div>
+                    <?php 
+                       $select_account = mysqli_query($conn, "SELECT * FROM `users`") or die('query failed');
+                       $number_of_account = mysqli_num_rows($select_account);
+                    ?>
+                    <div class="overall">
+                       <h1 class="card--value"><?php echo $number_of_account; ?></h1>
                     </div>
                 </div>
                 <div class="card card-1">
                     <div class="card--title">
                         <span class="card--icon icon"><i class='bx bx-chat'></i></span>
                         <span>New Messages</span>
+                    </div>
+                    <?php 
+                       $select_messages = mysqli_query($conn, "SELECT * FROM `message`") or die('query failed');
+                       $number_of_messages = mysqli_num_rows($select_messages);
+                    ?>
+                    <div class="overall">
+                       <h1 class="card--value"><?php echo $number_of_messages; ?></h1>
                     </div>
                 </div>
 
@@ -142,96 +151,6 @@ session_start();
     </section>
 
 
-    <div class="box-container">
-      <div class="box">
-         <?php
-            $total_pendings = 0;
-            $select_pending = mysqli_query($conn, "SELECT total_price FROM `orders` WHERE payement_status = 'pending'") or die('query failed');
-            if(mysqli_num_rows($select_pending) > 0){
-               while($fetch_pendings = mysqli_fetch_assoc($select_pending)){
-                  $total_price = $fetch_pendings['total_price'];
-                  $total_pendings += $total_price;
-               };
-            };
-         ?>
-         <h3>$<?php echo $total_pendings; ?>/-</h3>
-         <p>total pendings</p>
-      </div>
-
-      <div class="box">
-         <?php
-            $total_completed = 0;
-            $select_completed = mysqli_query($conn, "SELECT total_price FROM `orders` WHERE payement_status = 'completed'") or die('query failed');
-            if(mysqli_num_rows($select_completed) > 0){
-               while($fetch_completed = mysqli_fetch_assoc($select_completed)){
-                  $total_price = $fetch_completed['total_price'];
-                  $total_completed += $total_price;
-               };
-            };
-         ?>
-         <h3>$<?php echo $total_completed; ?>/-</h3>
-         <p>completed payments</p>
-      </div>
-
-      <div class="box">
-         <?php 
-            $select_orders = mysqli_query($conn, "SELECT * FROM `orders`") or die('query failed');
-            $number_of_orders = mysqli_num_rows($select_orders);
-         ?>
-         <h3><?php echo $number_of_orders; ?></h3>
-         <p>order placed</p>
-      </div>
-
-      <div class="box">
-         <?php 
-            $select_products = mysqli_query($conn, "SELECT * FROM `products`") or die('query failed');
-            $number_of_products = mysqli_num_rows($select_products);
-         ?>
-         <h3><?php echo $number_of_products; ?></h3>
-         <p>products added</p>
-      </div>
-
-      <div class="box">
-         <?php 
-            $select_users = mysqli_query($conn, "SELECT * FROM `users` WHERE user_type = 'user'") or die('query failed');
-            $number_of_users = mysqli_num_rows($select_users);
-         ?>
-         <h3><?php echo $number_of_users; ?></h3>
-         <p>normal users</p>
-      </div>
-
-      <div class="box">
-         <?php 
-            $select_admins = mysqli_query($conn, "SELECT * FROM `users` WHERE user_type = 'admin'") or die('query failed');
-            $number_of_admins = mysqli_num_rows($select_admins);
-         ?>
-         <h3><?php echo $number_of_admins; ?></h3>
-         <p>admin users</p>
-      </div>
-
-      <div class="box">
-         <?php 
-            $select_account = mysqli_query($conn, "SELECT * FROM `users`") or die('query failed');
-            $number_of_account = mysqli_num_rows($select_account);
-         ?>
-         <h3><?php echo $number_of_account; ?></h3>
-         <p>total accounts</p>
-      </div>
-
-      <div class="box">
-         <?php 
-            $select_messages = mysqli_query($conn, "SELECT * FROM `message`") or die('query failed');
-            $number_of_messages = mysqli_num_rows($select_messages);
-         ?>
-         <h3><?php echo $number_of_messages; ?></h3>
-         <p>new messages</p>
-      </div>
-
-    </div>
-
-
-
-<!-- admin dashboard section ends -->
 
     <script src="../js/admin_script.js"></script> 
 </body>
