e1bb1bdab78bf8c685228dc9aa1c8d31f8952f5f
cheifaikram <cheifaikram@gmail.com>
2023-06-26 03:57:25 +0100

    commit

--- admin/admin_page.php
+++ admin/admin_page.php
@@ -15,6 +15,63 @@ session_start();
     <?php include 'admin_header.php'; ?>
 <!-- admin dashboard section starts  -->
 
+<section class="main">
+        <div class="sidebar">
+            <ul class="sidebar--items">
+                <li>
+                    <a href="#" class="active">
+                        <span class="icon"><i class="ri-bar-chart-line"></i></span>
+                        <div class="sidebar--item">Overview</div>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <span class="icon"><i class="ri-handbag-line"></i></span>
+                        <div class="sidebar--item">Product</div>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <span class="icon"><i class="ri-user-line"></i></span>
+                        <div class="sidebar--item">Customers</div>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <span class="icon"><i class="ri-booklet-line"></i></span>
+                        <div class="sidebar--item">Orders</div>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <span class="icon"><i class="ri-shopping-cart-2-line"></i></span>
+                        <div class="sidebar--item">Checkout</div>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <span class="icon"><i class="ri-settings-3-line"></i></span>
+                        <div class="sidebar--item">Settings</div>
+                    </a>
+                </li>
+            </ul>
+            <ul class="sidebar--bottom--items">
+                <li>
+                    <a href="#">
+                        <span class="icon"><i class="ri-question-line"></i></span>
+                        <div class="sidebar--item">Help</div>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <span class="icon"><i class="ri-logout-box-r-line"></i></span>
+                        <div class="sidebar--item">Logout</div>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </section>
+
 <section class="dashboard">
     <h1 class="title">dashboard</h1>
     <div class="box-container">
