7a12e9e9eb2e5019769a6908eafc4c90b7b728e1
cheifaikram <cheifaikram@gmail.com>
2023-07-03 23:32:50 +0100

    commit

--- admin/admin_orders.php
+++ admin/admin_orders.php
@@ -1,11 +1,22 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+   <meta charset="UTF-8">
+   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11.0.19/dist/sweetalert2.min.css">
+   <link rel="stylesheet" href="../css/admin_style.css">
+
+   <script src="https://cdn.jsdelivr.net/npm/chart.js@3.9.1/dist/chart.min.js"></script>
+   <link href="https://cdn.jsdelivr.net/npm/remixicon@2.5.0/fonts/remixicon.css" rel="stylesheet">
+   <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
 </head>
-<body>
-    <h1>orders</h1>
+<body class="body">
+   <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.0.19/dist/sweetalert2.min.js"></script>
+   <?php include 'admin_head.php'; ?>
+   <section class="main">
+      <?php include 'admin_header.php'; ?>
+      <div class="order--container">
+        <h1>Lorem ipsum dolor sit amet.</h1>
+      </div>
+   </section>
 </body>
 </html>
