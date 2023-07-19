a04955f9cac1b43e75a3f7e882f397ba6b871330
cheifaikram <cheifaikram@gmail.com>
2023-07-19 21:06:46 +0100

    commit

--- header.php
+++ header.php
@@ -87,7 +87,7 @@ $current_page = basename($_SERVER['PHP_SELF']);
                   $select_cart_number = mysqli_query($conn, "SELECT * FROM `cart` WHERE user_id = '$user_id'") or die('query failed');
                   $cart_rows_number = mysqli_num_rows($select_cart_number); 
                   ?>
-                  <a href="cart.php" class="cart-logo"> <i class="ri-shopping-cart-2-line"></i> <span>(<?php echo $cart_rows_number; ?>)</span> </a>
+                  <a href="cart.php" class="cart-logo"> <i class="ri-shopping-cart-2-line cart"></i> <span>(<?php echo $cart_rows_number; ?>)</span> </a>
                 </div>
                 <?php
             } else{
